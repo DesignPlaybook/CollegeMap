84de71cb4ee36abc0edfe7100225e8a5a3a311e5
rishikesh <prishikesh18@gmail.com>
2025-05-06 01:38:08 +0530

    Update CORS configuration to allow requests from the correct frontend domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://college-map-ed0f662da28b.herokuapp.com'  # Your frontend's domain
+    origins 'https://www.collegemap.in'  # Your frontend's domain
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
