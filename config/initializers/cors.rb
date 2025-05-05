04a41830b61fca489b80d3dd5877b82ce368ea3b
rishikesh <prishikesh18@gmail.com>
2025-05-06 01:18:13 +0530

    Refactor CORS configuration to restrict origins and simplify resource settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,19 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://#{ENV["EXTERNAL_HOST"]}"
-
-    resource "*",
-      headers: :any,
-      expose: ["Authorization"], # Allow the Authorization header
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], 
-      credentials: true # Allow cookies to be sent cross-origin
-
+    origins 'https://www.collegemap.in'  # Your frontend's domain
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
