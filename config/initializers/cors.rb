5e7887ba7f71dbb0e7abfb9fcf2d97916e0d7358
rishikesh <prishikesh18@gmail.com>
2025-05-06 01:41:30 +0530

    Refactor CORS configuration for improved clarity and maintainability

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,10 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.collegemap.in'  # Your frontend's domain
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    origins 'https://www.collegemap.in'  # Allow only your frontend domain
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options],
+      credentials: true  # Allow credentials to be included in the requests
   end
 end
